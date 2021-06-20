7ea02a1166ad1005a7ed29e6f9f417f3b55dff98
Kshitij Kavimandan <kshitijk3188@gmail.com>
2021-06-21 01:05:41 +0530

    Creating rooms

--- src/Chat.js
+++ src/Chat.js
@@ -1,14 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { Avatar, IconButton } from "@material-ui/core";
 import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
+import MicIcon from "@material-ui/icons/Mic";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 import "./Chat.css";
 
 function Chat() {
     const[seed, setSeed] = useState('');
+    const[input, setInput] = useState('');
 
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5000))
     }, []);
+
+    const sendMessage = (e) => {
+        e.preventDefault();
+        setInput('');
+    }
 
     return (
         <div className='chat'>
@@ -34,7 +42,7 @@ function Chat() {
             </div>
 
             <div className="chat__body">
-                <p className="chat__message">
+                <p className={`chat__message ${true && 'chat__reciever'}`}>
                     <span className="chat__name">Kshitij</span>
                     Hey Guys
                     <span className="chat__timestamp">3:52pm</span>
@@ -42,7 +50,12 @@ function Chat() {
             </div>
 
             <div className="chat__footer">
-
+                <InsertEmoticonIcon />
+                <form>
+                    <input value={input} onChange={(e) => setInput(e.target.value)} placeholder="Type a message" type="text"/>
+                    <button onClick={sendMessage} type="submit">Send a message</button>
+                </form>
+                <MicIcon />
             </div>
         </div>
     )
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCcXyfDxVLNDORbYEQ72kKLd7LFG32-CJA",
@@ -8,3 +10,11 @@ const firebaseConfig = {
   appId: "1:984971410528:web:66f9b7605bba80f3ca9705",
   measurementId: "G-VNM08P8L0M"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
