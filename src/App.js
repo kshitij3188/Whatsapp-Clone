eae778a186c6be96b0f563f622889300dc1edb08
Kshitij Kavimandan <kshitijk3188@gmail.com>
2021-06-21 12:30:23 +0530

    Login page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
+import { useState } from 'react';
 import './App.css';
+import Login from './Login';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
     <div className="app">
+      {!user ? (
+        <Login />
+      ) : (
       <div className="app__body">
+        <Router>
         <Sidebar />
-        <Chat />
+          <Switch>
+            <Route path="/rooms/:roomId">
+              <Chat />
+            </Route>
+            <Route path="/">
+              <Chat />
+            </Route>
+          </Switch>
+        </Router>
       </div>
+      )}
     </div>
   );
 }
--- src/Chat.js
+++ src/Chat.js
@@ -3,15 +3,27 @@ import { Avatar, IconButton } from "@material-ui/core";
 import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
 import MicIcon from "@material-ui/icons/Mic";
 import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import { useParams } from 'react-router-dom';
 import "./Chat.css";
+import db from "./firebase";
 
 function Chat() {
     const[seed, setSeed] = useState('');
     const[input, setInput] = useState('');
+    const { roomId } = useParams();
+    const [roomName, setRoomName] = useState('');
+
+    useEffect(() => {
+        if (roomId) {
+            db.collection('rooms').doc(roomId).onSnapshot((snapshot) =>
+                setRoomName(snapshot.data().name)
+            );
+        }
+    }, [roomId])
 
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5000))
-    }, []);
+    }, [roomId]);
 
     const sendMessage = (e) => {
         e.preventDefault();
@@ -24,7 +36,7 @@ function Chat() {
                 <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
 
                 <div className="chat__headerInfo">
-                    <h3>Room name</h3>
+                    <h3>{roomName}</h3>
                     <p>Last seen at ...</p>
                 </div>
 
