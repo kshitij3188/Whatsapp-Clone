5f3f22045b6499f5be5ea8201ae2c827cee77091
Kshitij Kavimandan <kshitijk3188@gmail.com>
2021-06-24 10:43:13 +0530

    Revert "Final Commit"
    This reverts commit b30566ebe16d82ff1c4e372f266a780024ee2552.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,11 +6,17 @@ function Footer() {
   return (
     <div>
       <div className="footer">
-        <a className="" href="https://github.com/kshitij3188">
+        <a className="" href="https://github.com/robin025">
           <GitHubIcon className="github__logo" />
         </a>
+        <h3>Made My Robin Singh | 2020</h3>
         <p>
           <strong>Made Using React</strong>
+        </p>
+        <p>
+          I do not own WhatsApp brand & logo, this web app clone was created only
+          to learn & show my skills in creating web applications and will never
+          be used for commercial purposes.
         </p>
       </div>
     </div>
